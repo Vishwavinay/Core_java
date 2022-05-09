1eff4a285c96229b9fc31d1dfc53a9c27c594971
mhjpnlab <mhjpnlab@admin>
2022-05-09 13:25:38 +0530

    first commit

---
+++ demo/src/Testdemo.java
@@ -0,0 +1,10 @@
+import java.awt.datatransfer.SystemFlavorMap;
+
+
+public class Testdemo {
+	System.out.println("hii !\n, Welcome to java ......Ta-da!");
+
+}
+public static void main(String[] args){
+	
+}
